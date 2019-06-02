a7c985f756c5b5d1843cefaaaa74df33290a1ac4
Javier Finez <javierfinezdios@gmail.com>
2019-06-02 23:09:51 +0200

    hot reload in development

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -4,7 +4,7 @@ const baseConfig = require("./webpack.config.base");
 module.exports = merge(baseConfig, {
   mode: "development",
   devServer: {
-    port: 3003
+    port: 3006
   },
   devtool: "source-map"
 });
